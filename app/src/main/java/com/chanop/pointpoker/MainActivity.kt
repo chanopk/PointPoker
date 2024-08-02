55d43a47745ec2d35ee8c4e4d43a70059da22464
Chanop Khonnasee <chanop.k@cimbthai.com>
2024-08-02 10:26:39 +0700

    first commit

---
+++ app/src/main/java/com/chanop/pointpoker/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.chanop.pointpoker
+
+import android.content.ContentValues.TAG
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.chanop.pointpoker.ui.theme.PointPokerTheme
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PointPokerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .clickable {
+                                testFirebase()
+                            }
+                    )
+                }
+            }
+        }
+    }
+}
+
+fun testFirebase() {
+    val db = Firebase.firestore
+    val user = hashMapOf(
+        "first" to "Ada2",
+        "last" to "Lovelace2",
+        "born" to 18152,
+    )
+
+    // Add a new document with a generated ID
+    db.collection("users")
+        .add(user)
+        .addOnSuccessListener { documentReference ->
+            Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+        }
+        .addOnFailureListener { e ->
+            Log.w(TAG, "Error adding document", e)
+        }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PointPokerTheme {
+        Greeting("Android")
+    }
+}
